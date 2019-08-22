6069e0bb4e2f2928583846add6cac5433282b2f7
Blu Borghi <blu.borghi@gmail.com>
2019-08-22 10:40:16 +0200

    for cycle exercise

--- for/for_cycle.s
+++ for/for_cycle.s
@@ -12,6 +12,11 @@ _start:
 loop:
     blt t1, t0, exit # if t1 < t0 then exit
     
+    li a0, 0 #write on stdout
+    ld a1, t0
+    li a2, 8
+    li a7, 64 #sys write
+    ecall
     
 
     addi t0, t0, +1
